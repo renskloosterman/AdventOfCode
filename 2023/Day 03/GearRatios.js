bca9a10bb56fa5921360e2aaee293d476ec44034
Rens Kloosterman <renskloosterman2001@live.nl>
2023-12-06 21:12:40 +0100

    Day 3 - Part 1 + Part 2

--- 2023/Day 03/GearRatios.js
+++ 2023/Day 03/GearRatios.js
@@ -1,26 +1,75 @@
 import { getInput } from '../utils/fileReader.js'
 
-const input = getInput(import.meta.url, "inputTest.txt")
+const input = getInput(import.meta.url, "input.txt")
 
-partOne();
+console.log(`Answer part 1: ${partOne()}`);
+console.log(`Answer part 2: ${partTwo()}`);
+
 
 function partOne() {
-    let partialNumbers = []
-    const symbols = "@#$%&*/+=-"
-    for(let i = 0; i < input.length; i++) {
-        input[i] = input[i].split('')
+  let total = 0
+
+  for (let lineIndex = 0; lineIndex < input.length; lineIndex++) {
+    let numbers = []
+
+    let match = null;
+    let pattern = /\d+/g;
+    while ((match = pattern.exec(input[lineIndex])) !== null) {
+      numbers.push({ start: match.index, end: pattern.lastIndex, number: match[0] });
     }
-    
-    for(let y = 0; y < input.length; y++) {
-        for(let x = 0; x < input[0].length; x++) {
-            if(symbols.includes(input[y][x])) {
-                // if found a symbol
-                checkCircle(input, y, x)
+
+    for (let number of numbers) {
+      let partOfSum = false
+      for (let y = lineIndex - 1; y <= lineIndex + 1; y++) {
+        for (let x = number.start - 1; x <= number.end; x++) {
+          if (y >= 0 && y < input.length && x >= 0 && input[lineIndex].length > x) {
+            if (isNaN(parseInt(input[y][x])) && input[y][x] != '.') {
+              partOfSum = true
+            }
+          }
+        }
+      }
+      if (partOfSum) total += parseInt(number.number);
+    }
+  }
+
+  return total
+}
+
+function partTwo() {
+    let total = 0;
+    let map = [];
+
+    for(let lineIndex = 0; lineIndex < input.length; lineIndex++) {
+        let numbers = []
+        
+        let match = null;
+        let pattern = /\d+/g;
+        while((match = pattern.exec(input[lineIndex])) !== null) {
+            numbers.push({ start: match.index, end: pattern.lastIndex - 1, number: match[0] });
+        }
+
+        for(let number of numbers) {
+            for(let y = lineIndex - 1; y <= lineIndex + 1; y++) {
+                for(let x = number.start - 1; x <= number.end + 1; x++) {
+                    if(y >= 0 && y < input.length && x >= 0 && x < input[lineIndex].length) {
+                        if(input[y][x] == '*') {
+                            map.push({ x, y, number: parseInt(number.number) })
+                        }
+                    }
+                }
             }
         }
     }
-}
 
-function checkCircle(input, y, x) {
-    console.log(`Found symbol ${input[y][x]} at ${y+1},${x+1}`)
+    for(let y = 0; y < input.length; y++) {
+        for(let x = 0; x < input[y].length; x++) {
+            let selected = map.filter(el => el.x == x && el.y == y);
+            if(selected.length == 2) {
+                let nums = selected.map(el => el.number);
+                total += nums[0] * nums[1];
+            }
+        }
+    }
+    return total
 }
