d1aef4b7ad2747a256bfc5b2c2c9c16276caecd0
Rens Kloosterman <renskloosterman2001@live.nl>
2024-12-09 15:18:03 +0100

    Update CamelCards.js

--- 2023/Day 07/CamelCards.js
+++ 2023/Day 07/CamelCards.js
@@ -25,18 +25,18 @@ function partOne() {
             bid: bid,
             rank: rank
         }
-        hands.push(result)        
+        hands.push(result)
     })
-    hands.sort((a,b) => {
-        if(a.rank !== b.rank) {
+    hands.sort((a, b) => {
+        if (a.rank !== b.rank) {
             return b.rank - a.rank
         }
 
-        for(let i = 0; i < a.cards.length; i++) {
+        for (let i = 0; i < a.cards.length; i++) {
             const indexA = cardOrder.indexOf(a.cards[i]);
             const indexB = cardOrder.indexOf(b.cards[i])
 
-            if(indexA !== indexB) {
+            if (indexA !== indexB) {
                 return indexA - indexB
             }
         }
@@ -44,8 +44,8 @@ function partOne() {
     })
     // console.log(hands)
     let totalWinnings = 0
-    for(let i = 0; i < hands.length; i++) {
-        totalWinnings += (i+1)*hands[i].bid
+    for (let i = 0; i < hands.length; i++) {
+        totalWinnings += (i + 1) * hands[i].bid
     }
     return totalWinnings
 }
@@ -56,5 +56,97 @@ function count(c, a) {
 }
 
 function partTwo() {
-    return 't.b.d'
+    const cardOrder = 'J23456789TQKA';
+    let hands = []
+    input.forEach(line => {
+        const [hand, bid] = line.split(" ");
+        const counts = hand.split('').reduce(count, {})
+        const duplicates = Object.values(counts).reduce(count, {})
+        // console.log(hand)
+        if (counts.J > 0) {
+            // console.log(duplicates)
+            // console.log(`Hand: ${hand} has ${counts.J} jokers`)
+            let _continue = true;
+            if (duplicates[4] && counts.J != 4 && _continue) {
+                duplicates[4] -= 1;
+                duplicates[(4 + counts.J)] = (duplicates[4 + counts.J] || 0) + 1
+                _continue = false;
+            }
+            if (duplicates[3] && counts.J != 3 && _continue) {
+                duplicates[3] -= 1;
+                duplicates[(3 + counts.J)] = (duplicates[3 + counts.J] || 0) + 1
+                _continue = false;
+            }
+            if (duplicates[2] && _continue) {
+                if (duplicates[2] > 1) {
+                    duplicates[2] -= 1;
+                    duplicates[(2 + counts.J)] = (duplicates[2 + counts.J] || 0) + 1
+                    _continue = false;
+                }
+                if (duplicates[2] == 1 && counts.J != 2 && _continue) {
+                    duplicates[2] -= 1;
+                    duplicates[(2 + counts.J)] = (duplicates[2 + counts.J] || 0) + 1
+                    _continue = false;
+                }
+            }
+            if (duplicates[1] && counts.J != 2 && _continue) {
+                duplicates[1] -= 1;
+                duplicates[(1 + counts.J)] = (duplicates[1 + counts.J] || 0) + 1
+                _continue = false;
+            }
+        }
+        let rank =
+            (duplicates[5] && 1) ||
+            (duplicates[4] && 2) ||
+            (duplicates[3] && duplicates[2] && 3) ||
+            (duplicates[3] && 4) ||
+            (duplicates[2] > 1 && 5) ||
+            (duplicates[2] && 6) ||
+            7
+        // if (counts.J > 0) {
+        let s = (
+            rank == 1 && (`5 of a kind`) ||
+            rank == 2 && (`4 of a kind`) ||
+            rank == 3 && (`full house`) ||
+            rank == 4 && (`3 of a kind`) ||
+            rank == 5 && (`2 pairs`) ||
+            rank == 6 && (`1 pair`) ||
+            rank == 7 && (`high card`)
+        )
+        // }
+
+        const result = {
+            cards: hand,
+            type: s,
+            rank: rank,
+            bid: bid,
+        }
+        hands.push(result)
+        // console.clear()
+    })
+    hands.sort((a, b) => {
+        if (a.rank !== b.rank) {
+            return b.rank - a.rank
+        }
+
+        for (let i = 0; i < a.cards.length; i++) {
+            const indexA = cardOrder.indexOf(a.cards[i]);
+            const indexB = cardOrder.indexOf(b.cards[i])
+
+            if (indexA !== indexB) {
+                return indexA - indexB
+            }
+        }
+        return 0
+    })
+    hands.forEach((element) => {
+        //if (element.cards && element.cards.includes('J')) {
+          console.log(JSON.stringify(element));
+       // }
+      });
+    let totalWinnings = 0
+    for (let i = 0; i < hands.length; i++) {
+        totalWinnings += (i + 1) * hands[i].bid
+    }
+    return totalWinnings
 }
